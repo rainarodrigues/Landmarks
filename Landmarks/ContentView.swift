64550c169929763030e2705e8aeebd2fc61fff9a
Raina Rodrigues de Lima <rainarodriguesdelima@10-45-48-131.0200.ifce.br.iosda.org>
2022-05-12 11:13:41 -0300

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,31 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Raina Rodrigues de Lima on 12/05/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Raina Rodrigues de Lima on 12/05/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
